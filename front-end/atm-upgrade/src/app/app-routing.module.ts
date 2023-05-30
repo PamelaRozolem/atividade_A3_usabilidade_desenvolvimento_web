3ee1fd42db84eaecbf314f8232608b5d19fc8c8e
pamela.rozolem <pamela.rozolem@accenture.com>
2023-05-30 17:53:52 -0300

    mudando estrutura de pasta

---
+++ front-end/atm-upgrade/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./new-atm/new-atm.module').then(m => m.NewAtmModule)
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ front-end/atm-upgrade/src/app/new-atm/qr-code-debito/qr-code-debito.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qr-code-debito',
+  templateUrl: './qr-code-debito.component.html',
+  styleUrls: ['./qr-code-debito.component.scss'],
+})
+
+export class QrCodeDebitoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
+
+
